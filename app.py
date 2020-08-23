7f3e6fca445436922038a2e8451a710c1800f4e2
SingeRoi <bonjour_robert@outlook.com>
2020-08-23 22:04:32 +0000

    adds add_messages function and creates list

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
-from flask import Flask
+from flask import Flask, redirect
 app = Flask(__name__)
+messages = []
+
+
+def add_messages(username, message):
+    """Add messages to the `messages` list"""
+    messages.append("{}, {}".format(username, message))
 
 
 @app.route('/')
@@ -8,14 +14,17 @@ def index():
     return "To send a message use /USERNAME/MESSAGE"
 
 
-@app.route('/<username>')
+@app.route('/<username>')    
 def user(username):
-    return "Hi " + username
+    '''Display chat messages'''
+    return "Welcome, {0} - {1}".format(username, messages)
 
 
 @app.route('/<username>/<message>')
 def send_message(username, message):
-    return "{0}: {1}".format(username, message)
+    """Create a new message and redirect back to the chat page"""
+    add_messages(username, message)
+    return redirect("/" + username)
 
 
 # These two lines should always be at the end of your app.py file.
